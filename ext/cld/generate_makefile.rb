7c964eec990f8316899187cd953c8d80b1e6aa89
newtonapple <david.github@gmail.com>
2017-12-10 01:41:35 -0800

    Update CLD2 to master.

--- ext/cld/generate_makefile.rb
+++ ext/cld/generate_makefile.rb
@@ -1,13 +1,22 @@
 require 'mkmf'
 
 # HACK: mkmf doesn't support multiple subdirs for the same library
-$objs = ["internal/cldutil.o",
+$objs = [
+  "internal/cld2_generated_cjk_compatible.o",
+  "internal/cld2_generated_deltaoctachrome0122.o",
+  "internal/cld2_generated_distinctoctachrome0122.o",
+  "internal/cld2_generated_quadchrome0122_2.o",
+  "internal/cld_generated_cjk_delta_bi_4.o",
+  "internal/cld_generated_cjk_uni_prop_80.o",
+  "internal/cld_generated_score_quad_octa_0122_2.o",
+  "internal/cldutil.o",
   "internal/cldutil_shared.o",
   "internal/compact_lang_det.o",
   "internal/compact_lang_det_hint_code.o",
   "internal/compact_lang_det_impl.o",
   "internal/debug.o",
   "internal/fixunicodevalue.o",
+  "internal/generated_distinct_bi_0.o",
   "internal/generated_entities.o",
   "internal/generated_language.o",
   "internal/generated_ulscript.o",
@@ -17,15 +26,8 @@ $objs = ["internal/cldutil.o",
   "internal/scoreonescriptspan.o",
   "internal/tote.o",
   "internal/utf8statetable.o",
-  "internal/cld_generated_cjk_uni_prop_80.o",
-  "internal/cld2_generated_cjk_compatible.o",
-  "internal/cld_generated_cjk_delta_bi_4.o",
-  "internal/generated_distinct_bi_0.o",
-  "internal/cld2_generated_quadchrome0122_2.o",
-  "internal/cld2_generated_deltaoctachrome0122.o",
-  "internal/cld2_generated_distinctoctachrome0122.o",
-  "internal/cld_generated_score_quad_octa_0122_2.o",
-  "thunk.o"]
+  "thunk.o"
+]
 
 if have_library('stdc++')
   create_makefile('libcld2')
