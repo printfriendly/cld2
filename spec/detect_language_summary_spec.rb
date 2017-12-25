39e027001647bf41e3cc520d2865ec504658aa9e
David Dai <ddai@scribd.com>
2017-12-24 18:19:11 -0800

    Add specs for CLD2.detect_language_summary :chunks.

--- spec/detect_language_summary_spec.rb
+++ spec/detect_language_summary_spec.rb
@@ -4,32 +4,12 @@ describe CLD do
   describe ".detect_language_summary" do
     it_behaves_like "language detection method", :detect_language_summary
 
+
     describe ":top_langs attribute" do
+      it_behaves_like "detected language attribute", :top_langs
+
       context "when multiple languages are detected" do
         subject { CLD.detect_language_summary('今天是來最偉大的一天。 Time to do some amazing work and kick some asses!  私の友達を始めましょう。')[:top_langs] }
-
-        it { expect(subject.size).to eq 3 }
-
-        it 'contains the correct lang_ids' do
-          codes = subject.map{|lang| lang[:lang_id]}
-          expect(codes).to include CLD::Language[:CHINESE_T]
-          expect(codes).to include CLD::Language[:ENGLISH]
-          expect(codes).to include CLD::Language[:JAPANESE]
-        end
-
-        it 'contains the correct language codes' do
-          codes = subject.map{|lang| lang[:code]}
-          expect(codes).to include 'zh-Hant'
-          expect(codes).to include 'en'
-          expect(codes).to include 'ja'
-        end
-
-        it 'contains the correct language names' do
-          names = subject.map{|lang| lang[:name]}
-          expect(names).to include 'ChineseT'
-          expect(names).to include 'ENGLISH'
-          expect(names).to include 'Japanese'
-        end
 
         it 'has internal scores' do
           subject.each{|lang| expect(lang[:score]).to be > 1.0}
@@ -43,20 +23,6 @@ describe CLD do
       context "when a single language is detected" do
         subject { CLD.detect_language_summary('Hola mi amigo, ¡hoy va a ser épico!')[:top_langs] }
 
-        it { expect(subject.size).to eq 1 }
-
-        it 'contains the correct lang_id' do
-          expect(subject[0][:lang_id]).to eq CLD::Language[:SPANISH]
-        end
-
-        it 'contains the correct language code' do
-          expect(subject[0][:code]).to eq 'es'
-        end
-
-        it 'contains the correct language name' do
-          expect(subject[0][:name]).to eq 'SPANISH'
-        end
-
         it 'has internal score' do
           expect(subject[0][:score]).to be > 1.0
         end
@@ -65,10 +31,20 @@ describe CLD do
           expect(subject[0][:percent]).to be > 90
         end
       end
+    end
 
-      context "when a unknown language are detected" do
-        subject { CLD.detect_language_summary('(*(&*&%%&%')[:top_langs] }
-        it { expect(subject.size).to eq 0 }
+    describe ':chunks atrribute' do
+      it_behaves_like "detected language attribute", :chunks
+
+      context "when multiple languages are detected" do
+        subject { CLD.detect_language_summary('今天是來最偉大的一天。 Time to do some amazing work and kick some asses!  私の友達を始めましょう。')[:chunks]}
+
+        it 'contains contents of the detected languages in their respective substring chunks' do
+          contents = subject.map{|chunk| chunk[:content]}
+          expect(contents[0]).to eq '今天是來最偉大的一天。 '
+          expect(contents[1]).to eq 'Time to do some amazing work and kick some asses!  '
+          expect(contents[2]).to eq '私の友達を始めましょう。'
+        end
       end
     end
   end
--- spec/language_spec_helper.rb
+++ spec/language_spec_helper.rb
@@ -77,3 +77,55 @@ shared_examples 'language detection method' do |detection_method|
     it { expect(subject[:reliable]).to_not be true }
   end
 end
+
+shared_examples 'detected language attribute' do |attribute|
+  context "when multiple languages are detected" do
+    subject { CLD.detect_language_summary('今天是來最偉大的一天。 Time to do some amazing work and kick some asses!  私の友達を始めましょう。')[attribute] }
+
+    it { expect(subject.size).to eq 3 }
+
+    it 'contains the correct lang_ids' do
+      codes = subject.map{|lang| lang[:lang_id]}
+      expect(codes).to include CLD::Language[:CHINESE_T]
+      expect(codes).to include CLD::Language[:ENGLISH]
+      expect(codes).to include CLD::Language[:JAPANESE]
+    end
+
+    it 'contains the correct language codes' do
+      codes = subject.map{|lang| lang[:code]}
+      expect(codes).to include 'zh-Hant'
+      expect(codes).to include 'en'
+      expect(codes).to include 'ja'
+    end
+
+    it 'contains the correct language names' do
+      names = subject.map{|lang| lang[:name]}
+      expect(names).to include 'ChineseT'
+      expect(names).to include 'ENGLISH'
+      expect(names).to include 'Japanese'
+    end
+  end
+
+  context "when a single language is detected" do
+    subject { CLD.detect_language_summary('Hola mi amigo, ¡hoy va a ser épico!')[attribute] }
+
+    it { expect(subject.size).to eq 1 }
+
+    it 'contains the correct lang_id' do
+      expect(subject[0][:lang_id]).to eq CLD::Language[:SPANISH]
+    end
+
+    it 'contains the correct language code' do
+      expect(subject[0][:code]).to eq 'es'
+    end
+
+    it 'contains the correct language name' do
+      expect(subject[0][:name]).to eq 'SPANISH'
+    end
+  end
+
+  context "when a unknown language are detected" do
+    subject { CLD.detect_language_summary('(*(&*&%%&%')[attribute] }
+    it { expect(subject.size).to eq 0 }
+  end
+end
